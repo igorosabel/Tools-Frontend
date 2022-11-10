92b2515e74ae39a0b5d384ce10b0c5ef1849f0cf
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2022-11-10 09:50:57 +0100

    Quito flex-layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { routing, appRoutingProviders } from './app.routes';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatListModule } from '@angular/material/list';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { routing, appRoutingProviders } from "./app.routes";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { MatListModule } from "@angular/material/list";
+import { MatRadioModule } from "@angular/material/radio";
+import { MatSelectModule } from "@angular/material/select";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatTabsModule } from "@angular/material/tabs";
+import { MatToolbarModule } from "@angular/material/toolbar";
 
-import { AppComponent }        from './app.component';
-import { MainComponent }       from './pages/main/main.component';
-import { Sha1Component }       from './pages/sha1/sha1.component';
-import { CryptComponent }      from './pages/crypt/crypt.component';
-import { UrlencodeComponent }  from './pages/urlencode/urlencode.component';
-import { WindowSizeComponent } from './pages/window-size/window-size.component';
-import { UserAgentComponent }  from './pages/user-agent/user-agent.component';
+import { AppComponent } from "./app.component";
+import { MainComponent } from "./pages/main/main.component";
+import { Sha1Component } from "./pages/sha1/sha1.component";
+import { CryptComponent } from "./pages/crypt/crypt.component";
+import { UrlencodeComponent } from "./pages/urlencode/urlencode.component";
+import { WindowSizeComponent } from "./pages/window-size/window-size.component";
+import { UserAgentComponent } from "./pages/user-agent/user-agent.component";
 
-import { UaInfoDialog }  from './components/ua-info-dialog/ua-info-dialog.component';
+import { UaInfoDialog } from "./components/ua-info-dialog/ua-info-dialog.component";
 
-import { Sha1Pipe }      from './pipes/sha1.pipe';
-import { UrldecodePipe } from './pipes/urldecode.pipe';
+import { Sha1Pipe } from "./pipes/sha1.pipe";
+import { UrldecodePipe } from "./pipes/urldecode.pipe";
 
-import { UrlToolsService } from './services/url-tools.service';
-import { ApiService }      from './services/api.service';
+import { UrlToolsService } from "./services/url-tools.service";
+import { ApiService } from "./services/api.service";
 
 @NgModule({
   declarations: [
@@ -45,13 +44,12 @@ import { ApiService }      from './services/api.service';
     UserAgentComponent,
     UaInfoDialog,
     Sha1Pipe,
-    UrldecodePipe
+    UrldecodePipe,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    FlexLayoutModule,
     FormsModule,
     routing,
     MatToolbarModule,
@@ -65,13 +63,9 @@ import { ApiService }      from './services/api.service';
     MatDialogModule,
     MatInputModule,
     MatTabsModule,
-    MatSelectModule
+    MatSelectModule,
   ],
-  providers: [
-    appRoutingProviders,
-    UrlToolsService,
-    ApiService
-  ],
-  bootstrap: [AppComponent]
+  providers: [appRoutingProviders, UrlToolsService, ApiService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
