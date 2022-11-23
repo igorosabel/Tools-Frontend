b0138c2dd79b41e720fe9cbed3cbbe65f3ca6426
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2022-11-23 12:46:57 +0100

    Actualizo Angular CLI (14.2.9 -> 15.0.0), Angular (14.2.10 -> 15.0.1), Angular Material (14.2.7 -> 15.0.0) y Angular CDK (14.2.7 -> 15.0.0). Correcciones por cambios de diseño.

--- src/app/components/ua-info-dialog/ua-info-dialog.component.ts
+++ src/app/components/ua-info-dialog/ua-info-dialog.component.ts
@@ -1,18 +1,17 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Component, Inject } from "@angular/core";
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
 
 @Component({
-  selector: 'ua-info-dialog',
-  templateUrl: './html/ua-info-dialog.html',
+  selector: "ua-info-dialog",
+  templateUrl: "./ua-info-dialog.html",
 })
 export class UaInfoDialog {
-
   constructor(
     public uaInfoRef: MatDialogRef<UaInfoDialog>,
-    @Inject(MAT_DIALOG_DATA) public data: any) { }
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
 
   onNoClick(): void {
     this.uaInfoRef.close();
   }
-
 }
