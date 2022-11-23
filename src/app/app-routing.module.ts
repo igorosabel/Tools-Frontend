1ee2b9dfd013a114c537cc1319cf90a3f94f92bb
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2022-11-23 12:58:17 +0100

    Correcciones por cambios en el diseño

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { CryptComponent } from "./pages/crypt/crypt.component";
 import { MainComponent } from "./pages/main/main.component";
 import { Sha1Component } from "./pages/sha1/sha1.component";
-import { CryptComponent } from "./pages/crypt/crypt.component";
 import { UrlencodeComponent } from "./pages/urlencode/urlencode.component";
-import { WindowSizeComponent } from "./pages/window-size/window-size.component";
 import { UserAgentComponent } from "./pages/user-agent/user-agent.component";
+import { WindowSizeComponent } from "./pages/window-size/window-size.component";
 
 const routes: Routes = [
   { path: "", component: MainComponent },
@@ -17,7 +17,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: "legacy" })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
